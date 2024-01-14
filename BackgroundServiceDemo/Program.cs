2f47fd92060d001750bfe4fd2f34d13f19f2bb98
Amir Doosti <amirdoosti@hotmail.com>
2024-01-14 10:36:55 +0400

    Add project files.

---
+++ BackgroundServiceDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using BackgroundServiceDemo;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Background Service Demo!");
+
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                // I used here a technique called Singleton Registeration because
+                // I need to get access to instance of worker service later to be able to
+                // add message handler.
+                services.AddSingleton<Worker>();
+                services.AddHostedService(provider => provider.GetService<Worker>());
+            })
+            .Build();
+
+        var worker = host.Services.GetService<Worker>();
+        worker.MessageReceived += Worker_MessageReceived;
+
+        host.Run();
+
+        Console.WriteLine("Background Service Demo is terminated!");        
+    }
+
+    private static void Worker_MessageReceived(object? sender, string e)
+    {
+        Console.WriteLine(e);
+    }
+}
---
+++ BackgroundServiceDemo/Worker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+
+namespace BackgroundServiceDemo
+{
+    public class Worker : BackgroundService
+    {
+        public event EventHandler<string>? MessageReceived;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Random rnd = new Random();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Background task logic goes here
+                var num = rnd.Next(100);
+                OnMessageReceived($"Random number generated in background service: {num}!");
+
+                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+            }
+        }
+
+        protected virtual void OnMessageReceived(string message) 
+        { 
+            MessageReceived?.Invoke(this, message);
+        }
+    }
+}
